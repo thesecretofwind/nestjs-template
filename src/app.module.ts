82c893a01bb4e32987b6bc1c58c5808714388c6f
chengguang.li <Chengguang.Li@vertiv.com>
2024-03-18 16:50:39 +0800

    feat: 新增加载env依赖

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,11 @@ import { AppService } from './app.service';
 
 import { CoffeesModule } from './coffees/coffees.module';
 import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
+    ConfigModule.forRoot(),
     CoffeesModule,
     MongooseModule.forRoot('mongodb://localhost:27017/nest_base'),
   ],
