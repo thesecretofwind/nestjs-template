0248403db065b2ec22b6ba5dd5eab9b6215590c5
thesecretofwind <2586404691@qq.com>
2024-03-19 22:48:00 +0800

    feat： 新增参数装饰器

---
+++ src/common/decorators/protocol.decorator.ts
@@ -0,0 +1,13 @@
+import { ExecutionContext, createParamDecorator } from "@nestjs/common";
+import { Request } from "express";
+
+
+export const Protocol = createParamDecorator(
+  // data就是调用@Protocal(data)装饰器里面传递的值
+  (data: unknown, ctx: ExecutionContext) => {
+    console.log(data);
+    
+    const request = ctx.switchToHttp().getRequest<Request>();
+    return request.protocol;
+  }
+)
