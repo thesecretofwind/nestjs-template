1221fa90de76d1e5e06c6ea9e6f7fa5194728125
thesecretofwind <2586404691@qq.com>
2024-03-19 20:45:31 +0800

    feat: 新增拦截器

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,8 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { HttpExceptionFilter } from './common/filters/http-exception/http-exception.filter';
+import { WrapResponseInterceptor } from './common/interceptors/wrap-response.interceptor';
+import { TimeoutInterceptor } from './common/interceptors/timeout.interceptor';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -21,6 +23,8 @@ async function bootstrap() {
 
   app.useGlobalFilters(new HttpExceptionFilter());
   // app.useGlobalGuards(new ApiKeyGuard());
+
+  app.useGlobalInterceptors(new WrapResponseInterceptor(), new TimeoutInterceptor());
   await app.listen(3000);
 }
 bootstrap();
